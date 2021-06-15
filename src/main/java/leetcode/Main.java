1695468cd55d490a6205f0c870317b9f6729ffdd
jizhuhua <740291624@qq.com>
2021-06-15 19:22:46 +0800

    [2021.06.15][jzh][leetcode]

--- src/main/java/leetcode/Main.java
+++ src/main/java/leetcode/Main.java
@@ -41,9 +41,88 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    }
 
+    private static Map<Integer, Room> map = new HashMap<>();
+
+    public static boolean addRoom(int id, int area, int price, int rooms, int[] address) {
+        if (map.containsKey(id)) {
+            map.put(id, new Room(area, price, rooms, address));
+            return false;
+        } else {
+            map.put(id, new Room(area, price, rooms, address));
+            return true;
         }
     }
 
+    private static boolean deleteRoom(int id) {
+        if (map.containsKey(id)) {
+            map.remove(id);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private static int[] queryRoom(int area, int price, int rooms, int[] address, int[][] orderBy) {
+        List<Room> filterRoom = new ArrayList<>();
+        for (Map.Entry<Integer, Room> integerRoomEntry : map.entrySet()) {
+            Room value = integerRoomEntry.getValue();
+            if (value.area >= area && value.price <= price && value.rooms == rooms) {
+                value.setId(integerRoomEntry.getKey());
+                filterRoom.add(value);
+            }
+        }
+        return filterRoom.stream().sorted(new Comparator<Room>() {
+            @Override
+            public int compare(Room o1, Room o2) {
+                int res = 0;
+                for (int[] ints : orderBy) {
+                    if (res != 0) {
+                        break;
+                    }
+                    int rule = ints[0];
+                    int upDown = ints[1];
+                    switch (rule) {
+                        case 1:
+                            res = upDown == 1 ? o1.area - o2.area : o2.area - o1.area;
+                            break;
+                        case 2:
+                            res = upDown == 1 ? o1.price - o2.price : o2.price - o1.price;
+                            break;
+                        case 3:
+                            int way1 = cal(o1.address, address);
+                            int way2 = cal(o2.address, address);
+                            res = upDown == 1 ? way1 - way2 : way2 - way1;
+                            break;
+                    }
+                }
+                return res == 0 ? o1.id - o2.id : o2.id - o1.id;
+            }
+        }).mapToInt(room -> room.id).toArray();
+    }
+
+    public static int cal(int[] point, int[] desPoint) {
+        return Math.abs(point[0] - desPoint[0]) + Math.abs(point[1] - desPoint[1]);
+    }
+}
+
+class Room {
+    int id;
+    int area;
+    int price;
+    int rooms;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    int[] address;
+
+    public Room(int area, int price, int rooms, int[] address) {
+        this.area = area;
+        this.price = price;
+        this.rooms = rooms;
+        this.address = address;
+    }
 }
